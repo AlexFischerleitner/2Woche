fdb1205920ec819b96eb174d56d976016783d0a0
Fischerleitner Alex <fischerleitner03@gmail.com>
2020-09-28 13:27:06 +0200

    print forEach

--- src/htlgkr/woche2/afischerleitner17/HalloJavaMitForEachh.java
+++ src/htlgkr/woche2/afischerleitner17/HalloJavaMitForEachh.java
@@ -5,10 +5,33 @@
  */
 package htlgkr.woche2.afischerleitner17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author User
  */
 public class HalloJavaMitForEachh {
+    List<String> java = new ArrayList<>();
     
+    public HalloJavaMitForEachh(){
+        
+    }
+    
+    public void printList(){
+        this.java.add("lost");
+        this.java.add("Ich");
+        
+        for(String s: java){
+            System.out.println(s);
+        }
+    }
+    
+    public void printListforEach(){
+        this.java.add("lost");
+        this.java.add("Ich");
+        
+        this.java.forEach((String s) -> System.out.println(s));
+    }
 }
