be4b808db98b17542c9d85df5d8c2fabc21eb59c
Fischerleitner Alex <fischerleitner03@gmail.com>
2020-09-28 14:34:58 +0200

    testFile

--- src/htlgkr/woche2/afischerleitner17/NumberTester.java
+++ src/htlgkr/woche2/afischerleitner17/NumberTester.java
@@ -5,10 +5,49 @@
  */
 package htlgkr.woche2.afischerleitner17;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
 public class NumberTester {
-    
+
+    String fileName;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTester oddTester) {
+
+    }
+
+    public void setPrimeTester(NumberTester primeTester) {
+
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester) {
+
+    }
+
+    public void testFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("number.txt"))) {
+            String line = br.readLine();
+            while(line!=null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
